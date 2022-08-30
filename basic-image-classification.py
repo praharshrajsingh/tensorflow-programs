ec28fdf97a614d617fc2b0e823a7b9d71a251b0e
praharshrajsingh <bunniprsnl@gmail.com>
2022-08-30 21:19:43 +0530

    Complete Program,
    -added plot_image() and plot_value_array()
    -added test runs on the model

--- basic-image-classification.py
+++ basic-image-classification.py
@@ -5,6 +5,10 @@ import matplotlib.pyplot as plt
 
 #print(tf.__version__)
 
+num_rows = 5
+num_cols = 3
+num_images = num_cols*num_rows
+
 fashion_mnist = tf.keras.datasets.fashion_mnist             #using fashion MNIST dataset which has 70k images
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
@@ -66,4 +70,58 @@ probability_model = tf.keras.Sequential([model,
                                          tf.keras.layers.Softmax()])
 predictions = probability_model.predict(test_images)
 
-print(np.argmax(predictions[0]))
+#print(np.argmax(predictions[0]))
+
+#print(test_label[0])
+
+def plot_image(i, predictions_array, true_label, img):
+    true_label, img = true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap = plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100*np.max(predictions_array),
+                                         class_names[true_label]),
+                                         color = color
+                                         )
+def plot_value_array(i, predictions_array, true_label):
+    true_label = true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color = '#777777')
+    plt.ylim([0,1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+#plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+#for i in range(num_images):
+#    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+#    plot_image(i, predictions[i], test_labels, test_images)
+#    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+ #   plot_value_array(i, predictions[i], test_labels)
+#plt.tight_layout()
+#plt.show()
+img = test_images[1]
+img = (np.expand_dims(img,0))
+#print(img.shape)
+
+prediction_single = probability_model.predict(img)
+
+#print(prediction_single)
+plot_image(1, prediction_single[0], test_labels, test_images)
+plt.show()
+plot_value_array(1, prediction_single[0], test_labels)
+_ = plt.xticks(range(10), class_names, rotation = 45)
+plt.show()
