3ad95733b3c33d3b9a6932cce01e03349c2e2452
praharshrajsingh <bunniprsnl@gmail.com>
2023-01-11 11:07:44 +0530

    changes

--- basic-image-classification.py
+++ basic-image-classification.py
@@ -105,14 +105,14 @@ def plot_value_array(i, predictions_array, true_label):
     thisplot[predicted_label].set_color('red')
     thisplot[true_label].set_color('blue')
 
-#plt.figure(figsize=(2*2*num_cols, 2*num_rows))
-#for i in range(num_images):
-#    plt.subplot(num_rows, 2*num_cols, 2*i+1)
-#    plot_image(i, predictions[i], test_labels, test_images)
-#    plt.subplot(num_rows, 2*num_cols, 2*i+2)
- #   plot_value_array(i, predictions[i], test_labels)
-#plt.tight_layout()
-#plt.show()
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+   plt.subplot(num_rows, 2*num_cols, 2*i+1)
+   plot_image(i, predictions[i], test_labels, test_images)
+   plt.subplot(num_rows, 2*num_cols, 2*i+2)
+   plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
 img = test_images[1]
 img = (np.expand_dims(img,0))
 #print(img.shape)
